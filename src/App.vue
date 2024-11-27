3904be6bc3ef9e8718d5e8ac3b354381bd236ded
moritz <decknamec@gmail.com>
2024-11-27 12:39:24 +0100

    Refactor App.vue to streamline user authentication and add AdminLayout component for admin checks

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,13 @@
 import Todos from "./components/Todos.vue";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
-import { ref, onMounted } from "vue";
+import { onMounted } from "vue";
+import AdminLayout from "./components/AdminLayout.vue";
 
 const auth = useAuthenticator();
-const user = ref(null);
-const isAdmin = ref(false);
-
-onMounted(async () => {
-  const currentUser = await auth.user;
-  if (currentUser) {
-    const groups =
-      currentUser.signInDetails?.attributes["cognito:groups"] || [];
-    isAdmin.value = groups.includes("ADMINS"); // Überprüfen, ob der Benutzer in der Admin-Gruppe ist
-  }
+const user = auth;
+onMounted(() => {
+  console.log("user");
 });
 </script>
 
@@ -23,10 +17,10 @@ onMounted(async () => {
     <authenticator>
       <template v-slot="{ user, signOut }">
         <h1>Hello {{ user?.signInDetails?.loginId }}'s todos</h1>
+        <h2>{{ user.signInDetails }}</h2>
         <Todos />
+        <AdminLayout />
         <button @click="signOut">Sign Out</button>
-        <p v-if="isAdmin">You are an admin.</p>
-        <p v-else>You are not an admin.</p>
       </template>
     </authenticator>
   </main>
