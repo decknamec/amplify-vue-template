bfdb2bd02ba8f5090fd0c6b7f7f7a5753b2165b0
moritz <decknamec@gmail.com>
2024-11-28 11:51:26 +0100

    automatic FastAPI Backend Deploy

--- amplify/myPythonFunction/handler.py
+++ amplify/myPythonFunction/handler.py
@@ -1,8 +1,20 @@
-def handler(event, context):
-    
-    print("Received event: " + str(event))
-    
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda!'
-    }
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
+
+app = FastAPI()
+
+# CORS aktivieren (wichtig für die Kommunikation mit Nuxt)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://main.d3sa6211036fsg.amplifyapp.com"],  # URL des Nuxt-Projekts
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI!"}
+
+handler = Mangum(app)
