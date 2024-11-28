ec28334e6cf90e0fa6c4763fcce0912142344eca
moritz <decknamec@gmail.com>
2024-11-28 13:24:25 +0100

    Update API request in MyComponent to use custom Lambda function

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -21,9 +21,14 @@ export default defineComponent({
   methods: {
     async fetchData() {
       try {
-        // API-Request mit Axios
-        const response = await axios.get(
+        // API-Request mit Axios LAMBDA FUNC
+        /*const response = await axios.get(
           "https://hqewww6uyof7umxy6fivskqrfi0yewrh.lambda-url.eu-central-1.on.aws/"
+        ); */
+
+        // this is our own lambda function in node js
+        const response = await axios.get(
+          "https://dj3d6pl3xm6b2x5bkds5yqzm6i0qrrep.lambda-url.eu-central-1.on.aws"
         );
         // Setze die Antwort in responseData
         this.responseData = response.data;
