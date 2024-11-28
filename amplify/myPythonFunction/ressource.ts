50454e5b4694d132efedd4106555bf9a4a649ae6
moritz <decknamec@gmail.com>
2024-11-28 12:35:34 +0100

    ?

--- amplify/myPythonFunction/ressource.ts
+++ amplify/myPythonFunction/ressource.ts
@@ -2,5 +2,5 @@ import { defineFunction } from "@aws-amplify/backend";
 
 export const myPythonFunction = defineFunction({
   name: "myPyhtonFunction",
-  entry: "./handler.js",
+  entry: "./handler.py",
 });
